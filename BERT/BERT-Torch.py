a273b246345bdbb29691ff82936fbb0ae988821d
Shuvendu Bikash <bikash.shuvendu@gmail.com>
2019-09-04 01:29:02 +0600

    bert: pytorch_bert implementation

--- BERT/BERT-Torch.py
+++ BERT/BERT-Torch.py
@@ -1,8 +1,8 @@
-'''
+"""
   code by Tae Hwan Jung(Jeff Jung) @graykode
   Reference : https://github.com/jadore801120/attention-is-all-you-need-pytorch
               https://github.com/JayParks/transformer, https://github.com/dhlee347/pytorchic-bert
-'''
+"""
 import math
 import re
 from random import *
@@ -15,11 +15,11 @@ from torch.autograd import Variable
 # BERT Parameters
 maxlen = 30
 batch_size = 6
-max_pred = 5 # max tokens of prediction
+max_pred = 5  # max tokens of prediction
 n_layers = 6
 n_heads = 12
 d_model = 768
-d_ff = 768*4 # 4*d_model, FeedForward dimension
+d_ff = 768 * 4  # 4*d_model, FeedForward dimension
 d_k = d_v = 64  # dimension of K(=Q), V
 n_segments = 2
 
@@ -31,9 +31,9 @@ text = (
     'Oh Congratulations, Juliet\n'
     'Thanks you Romeo'
 )
-sentences = re.sub("[.,!?\\-]", '', text.lower()).split('\n') # filter '.', ',', '?', '!'
+sentences = re.sub("[.,!?\\-]", '', text.lower()).split('\n')  # filter '.', ',', '?', '!'
 word_list = list(set(" ".join(sentences).split()))
-word_dict = {'[PAD]' : 0, '[CLS]' : 1, '[SEP]' : 2, '[MASK]' : 3}
+word_dict = {'[PAD]': 0, '[CLS]': 1, '[SEP]': 2, '[MASK]': 3}
 for i, w in enumerate(word_list):
     word_dict[w] = i + 4
 number_dict = {i: w for i, w in enumerate(word_dict)}
@@ -44,18 +44,20 @@ for sentence in sentences:
     arr = [word_dict[s] for s in sentence.split()]
     token_list.append(arr)
 
+
 # sample IsNext and NotNext to be same in small batch size
 def make_batch():
     batch = []
     positive = negative = 0
-    while positive != batch_size/2 or negative != batch_size/2:
-        tokens_a_index, tokens_b_index= randrange(len(sentences)), randrange(len(sentences)) # sample random index in sentences
-        tokens_a, tokens_b= token_list[tokens_a_index], token_list[tokens_b_index]
+    while positive != batch_size / 2 or negative != batch_size / 2:
+        tokens_a_index, tokens_b_index = randrange(len(sentences)), randrange(
+            len(sentences))  # sample random index in sentences
+        tokens_a, tokens_b = token_list[tokens_a_index], token_list[tokens_b_index]
         input_ids = [word_dict['[CLS]']] + tokens_a + [word_dict['[SEP]']] + tokens_b + [word_dict['[SEP]']]
         segment_ids = [0] * (1 + len(tokens_a) + 1) + [1] * (len(tokens_b) + 1)
 
         # MASK LM
-        n_pred =  min(max_pred, max(1, int(round(len(input_ids) * 0.15)))) # 15 % of tokens in one sentence
+        n_pred = min(max_pred, max(1, int(round(len(input_ids) * 0.15))))  # 15 % of tokens in one sentence
         cand_maked_pos = [i for i, token in enumerate(input_ids)
                           if token != word_dict['[CLS]'] and token != word_dict['[SEP]']]
         shuffle(cand_maked_pos)
@@ -64,10 +66,10 @@ def make_batch():
             masked_pos.append(pos)
             masked_tokens.append(input_ids[pos])
             if random() < 0.8:  # 80%
-                input_ids[pos] = word_dict['[MASK]'] # make mask
+                input_ids[pos] = word_dict['[MASK]']  # make mask
             elif random() < 0.5:  # 10%
-                index = randint(0, vocab_size - 1) # random index in vocabulary
-                input_ids[pos] = word_dict[number_dict[index]] # replace
+                index = randint(0, vocab_size - 1)  # random index in vocabulary
+                input_ids[pos] = word_dict[number_dict[index]]  # replace
 
         # Zero Paddings
         n_pad = maxlen - len(input_ids)
@@ -80,14 +82,16 @@ def make_batch():
             masked_tokens.extend([0] * n_pad)
             masked_pos.extend([0] * n_pad)
 
-        if tokens_a_index + 1 == tokens_b_index and positive < batch_size/2:
-            batch.append([input_ids, segment_ids, masked_tokens, masked_pos, True]) # IsNext
+        if tokens_a_index + 1 == tokens_b_index and positive < batch_size / 2:
+            batch.append([input_ids, segment_ids, masked_tokens, masked_pos, True])  # IsNext
             positive += 1
-        elif tokens_a_index + 1 != tokens_b_index and negative < batch_size/2:
-            batch.append([input_ids, segment_ids, masked_tokens, masked_pos, False]) # NotNext
+        elif tokens_a_index + 1 != tokens_b_index and negative < batch_size / 2:
+            batch.append([input_ids, segment_ids, masked_tokens, masked_pos, False])  # NotNext
             negative += 1
     return batch
-# Proprecessing Finished
+
+
+# Preprecessing Finished
 
 def get_attn_pad_mask(seq_q, seq_k):
     batch_size, len_q = seq_q.size()
@@ -96,9 +100,11 @@ def get_attn_pad_mask(seq_q, seq_k):
     pad_attn_mask = seq_k.data.eq(0).unsqueeze(1)  # batch_size x 1 x len_k(=len_q), one is masking
     return pad_attn_mask.expand(batch_size, len_q, len_k)  # batch_size x len_q x len_k
 
+
 def gelu(x):
     "Implementation of the gelu activation function by Hugging Face"
     return x * 0.5 * (1.0 + torch.erf(x / math.sqrt(2.0)))
+
 
 class Embedding(nn.Module):
     def __init__(self):
@@ -115,16 +121,19 @@ class Embedding(nn.Module):
         embedding = self.tok_embed(x) + self.pos_embed(pos) + self.seg_embed(seg)
         return self.norm(embedding)
 
+
 class ScaledDotProductAttention(nn.Module):
     def __init__(self):
         super(ScaledDotProductAttention, self).__init__()
 
     def forward(self, Q, K, V, attn_mask):
-        scores = torch.matmul(Q, K.transpose(-1, -2)) / np.sqrt(d_k) # scores : [batch_size x n_heads x len_q(=len_k) x len_k(=len_q)]
-        scores.masked_fill_(attn_mask, -1e9) # Fills elements of self tensor with value where mask is one.
+        scores = torch.matmul(Q, K.transpose(-1, -2)) / np.sqrt(
+            d_k)  # scores : [batch_size x n_heads x len_q(=len_k) x len_k(=len_q)]
+        scores.masked_fill_(attn_mask, -1e9)  # Fills elements of self tensor with value where mask is one.
         attn = nn.Softmax(dim=-1)(scores)
         context = torch.matmul(attn, V)
         return context, attn
+
 
 class MultiHeadAttention(nn.Module):
     def __init__(self):
@@ -132,6 +141,7 @@ class MultiHeadAttention(nn.Module):
         self.W_Q = nn.Linear(d_model, d_k * n_heads)
         self.W_K = nn.Linear(d_model, d_k * n_heads)
         self.W_V = nn.Linear(d_model, d_v * n_heads)
+
     def forward(self, Q, K, V, attn_mask):
         # q: [batch_size x len_q x d_model], k: [batch_size x len_k x d_model], v: [batch_size x len_k x d_model]
         residual, batch_size = Q, Q.size(0)
@@ -146,7 +156,8 @@ class MultiHeadAttention(nn.Module):
         context, attn = ScaledDotProductAttention()(q_s, k_s, v_s, attn_mask)
         context = context.transpose(1, 2).contiguous().view(batch_size, -1, n_heads * d_v) # context: [batch_size x len_q x n_heads * d_v]
         output = nn.Linear(n_heads * d_v, d_model)(context)
-        return nn.LayerNorm(d_model)(output + residual), attn # output: [batch_size x len_q x d_model]
+        return nn.LayerNorm(d_model)(output + residual), attn  # output: [batch_size x len_q x d_model]
+
 
 class PoswiseFeedForwardNet(nn.Module):
     def __init__(self):
@@ -158,6 +169,7 @@ class PoswiseFeedForwardNet(nn.Module):
         # (batch_size, len_seq, d_model) -> (batch_size, len_seq, d_ff) -> (batch_size, len_seq, d_model)
         return self.fc2(gelu(self.fc1(x)))
 
+
 class EncoderLayer(nn.Module):
     def __init__(self):
         super(EncoderLayer, self).__init__()
@@ -165,9 +177,11 @@ class EncoderLayer(nn.Module):
         self.pos_ffn = PoswiseFeedForwardNet()
 
     def forward(self, enc_inputs, enc_self_attn_mask):
-        enc_outputs, attn = self.enc_self_attn(enc_inputs, enc_inputs, enc_inputs, enc_self_attn_mask) # enc_inputs to same Q,K,V
-        enc_outputs = self.pos_ffn(enc_outputs) # enc_outputs: [batch_size x len_q x d_model]
+        enc_outputs, attn = self.enc_self_attn(enc_inputs, enc_inputs, enc_inputs,
+                                               enc_self_attn_mask)  # enc_inputs to same Q,K,V
+        enc_outputs = self.pos_ffn(enc_outputs)  # enc_outputs: [batch_size x len_q x d_model]
         return enc_outputs, attn
+
 
 class BERT(nn.Module):
     def __init__(self):
@@ -194,15 +208,16 @@ class BERT(nn.Module):
             output, enc_self_attn = layer(output, enc_self_attn_mask)
         # output : [batch_size, len, d_model], attn : [batch_size, n_heads, d_mode, d_model]
         # it will be decided by first token(CLS)
-        h_pooled = self.activ1(self.fc(output[:, 0])) # [batch_size, d_model]
-        logits_clsf = self.classifier(h_pooled) # [batch_size, 2]
+        h_pooled = self.activ1(self.fc(output[:, 0]))  # [batch_size, d_model]
+        logits_clsf = self.classifier(h_pooled)  # [batch_size, 2]
 
         masked_pos = masked_pos[:, :, None].expand(-1, -1, output.size(-1)) # [batch_size, maxlen, d_model]
         h_masked = torch.gather(output, 1, masked_pos) # masking position [batch_size, len, d_model]
         h_masked = self.norm(self.activ2(self.linear(h_masked)))
-        logits_lm = self.decoder(h_masked) + self.decoder_bias # [batch_size, maxlen, n_vocab]
+        logits_lm = self.decoder(h_masked) + self.decoder_bias  # [batch_size, maxlen, n_vocab]
 
         return logits_lm, logits_clsf
+
 
 model = BERT()
 criterion = nn.CrossEntropyLoss()
@@ -211,15 +226,15 @@ optimizer = optim.Adam(model.parameters(), lr=0.001)
 batch = make_batch()
 input_ids, segment_ids, masked_tokens, masked_pos, isNext = zip(*batch)
 input_ids, segment_ids, masked_tokens, masked_pos, isNext = \
-    torch.LongTensor(input_ids),  torch.LongTensor(segment_ids), torch.LongTensor(masked_tokens), \
+    torch.LongTensor(input_ids), torch.LongTensor(segment_ids), torch.LongTensor(masked_tokens), \
     torch.LongTensor(masked_pos), torch.LongTensor(isNext)
 
 for epoch in range(100):
     optimizer.zero_grad()
     logits_lm, logits_clsf = model(input_ids, segment_ids, masked_pos)
-    loss_lm = criterion(logits_lm.transpose(1, 2), masked_tokens) # for masked LM
+    loss_lm = criterion(logits_lm.transpose(1, 2), masked_tokens)  # for masked LM
     loss_lm = (loss_lm.float()).mean()
-    loss_clsf = criterion(logits_clsf, isNext) # for sentence classification
+    loss_clsf = criterion(logits_clsf, isNext)  # for sentence classification
     loss = loss_lm + loss_clsf
     if (epoch + 1) % 10 == 0:
         print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.6f}'.format(loss))
@@ -234,9 +249,9 @@ print([number_dict[w] for w in input_ids if number_dict[w] != '[PAD]'])
 logits_lm, logits_clsf = model(torch.LongTensor([input_ids]), \
                                torch.LongTensor([segment_ids]), torch.LongTensor([masked_pos]))
 logits_lm = logits_lm.data.max(2)[1][0].data.numpy()
-print('masked tokens list : ',[pos for pos in masked_tokens if pos != 0])
-print('predict masked tokens list : ',[pos for pos in logits_lm if pos != 0])
+print('masked tokens list : ', [pos for pos in masked_tokens if pos != 0])
+print('predict masked tokens list : ', [pos for pos in logits_lm if pos != 0])
 
 logits_clsf = logits_clsf.data.max(1)[1].data.numpy()[0]
 print('isNext : ', True if isNext else False)
-print('predict isNext : ',True if logits_clsf else False)
+print('predict isNext : ', True if logits_clsf else False)
